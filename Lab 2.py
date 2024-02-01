876078f37ca7f484078b0f4331b3b632b9f9c565
amacrowski01 <157068736+amacrowski01@users.noreply.github.com>
2024-02-01 16:21:10 -0500

    Add files via upload

---
+++ Lab 2.py
@@ -0,0 +1,28 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #the authors' names are Allison Macrowski and Therese Burns
+>>> 
+================ RESTART: C:/Users/almac/OneDrive/Desktop/L1.py ================
+3
+The calculated amount is  18
+0
+1.0
+1
+>>> 
+================ RESTART: C:/Users/almac/OneDrive/Desktop/L2.py ================
+x=  3.141592653589793
+y=  1.2246467991473532e-16
+z=  0.7853981633974483
+6.3639610306789285
+>>> 
+================ RESTART: C:/Users/almac/OneDrive/Desktop/L3.py ================
+Traceback (most recent call last):
+  File "C:/Users/almac/OneDrive/Desktop/L3.py", line 1, in <module>
+    a=math.tan(math.pi/4)
+NameError: name 'math' is not defined. Did you forget to import 'math'?
+>>> 
+================ RESTART: C:/Users/almac/OneDrive/Desktop/L3.py ================
+0.8773826753016618
+>>> 
+================ RESTART: C:/Users/almac/OneDrive/Desktop/L4.py ================
+18.265482457436683
